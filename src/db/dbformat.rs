40b4a1833b093ed4d345581825ef0957f6c82dd7
ph <PatrickHuang888@users.noreply.github.com>
2025-06-22 13:23:12 +0800

    把encode/decode varint32移动到util目录

--- src/db/dbformat.rs
+++ src/db/dbformat.rs
@@ -156,27 +156,3 @@ impl TableKey {
         &self.value
     }
 }
-
-pub(super) fn decode_varint32(buf: &[u8]) -> Option<(u32, usize)> {
-    let mut result = 0u32;
-    let mut shift = 0;
-    for (i, &b) in buf.iter().enumerate() {
-        result |= ((b & 0x7F) as u32) << shift;
-        if b < 0x80 {
-            return Some((result, i + 1));
-        }
-        shift += 7;
-        if shift > 28 {
-            return None; // varint32 太长
-        }
-    }
-    None
-}
-
-pub(super) fn encode_varint32(buf: &mut Vec<u8>, mut v: u32) {
-    while v >= 0x80 {
-        buf.push((v as u8) | 0x80);
-        v >>= 7;
-    }
-    buf.push(v as u8);
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,25 @@
 pub mod db;
+pub mod table;
+pub(crate) mod util;
+
+#[derive(Clone)]
+pub struct Options {
+    // Number of keys between restart points for delta encoding of keys.
+    // This parameter can be changed dynamically.  Most clients should
+    // leave this parameter alone.
+    pub block_restart_interval: usize,
+    // Approximate size of user data packed per block.  Note that the
+    // block size specified here corresponds to uncompressed data.  The
+    // actual size of the unit read from disk may be smaller if
+    // compression is enabled.  This parameter can be changed dynamically.
+    pub block_size: usize,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options {
+            block_restart_interval: 16,
+            block_size: 4096,
+        }
+    }
+}
